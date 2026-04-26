cbcfd2120d13860a2ad109a8e65c07c38f09cbb1
AnhKhoi <khoina231@gmail.com>
2026-04-26 12:05:52 +0700

    chore: remove unused files and associated references

--- src/main/java/kna/springsecurity/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package kna.springsecurity.repository;
-
-import kna.springsecurity.entity.User;
-import java.util.Optional;
-
-public interface UserRepository {
-    Optional<User> findByUsername(String username);
-    User save(User user);
-}
